7429267ffea347a3dd7a26c769b240191b27adff
YouGuessedMyName <ivo@melse.info>
2025-05-17 20:37:23 +0200

    progress

--- engine.py
+++ engine.py
@@ -1,14 +1,75 @@
-def out(s: str):
-    print(s)
+import copy
+from time import sleep
+from typing import Tuple
 
-def inp_int() -> int:
-    try:
-        return(int(input()))
-    except Exception:
-        print("Expected an integer.")
+WAIT_NORMAL = 1.5
+WAIT_SHORT = 1
+WAIT_TINY = 0.5
 
+error_messages: dict = None
 
-def choice(options: list[str]) -> int:
-    for i, option in enumerate(options):
-        print(f"{i}) {option}")
-    return inp_int()
+def set_error_messages(messages: dict) -> None:
+    global error_messages
+    error_messages = messages
+
+def out(*args, **kwargs) -> None:
+    print(*args, **kwargs)
+
+def play_scene(scene: list[list[str]], do_inp: bool=True, wait: int = WAIT_NORMAL) -> None:
+    for frag in scene:
+        for line in frag:
+            out(line)
+            sleep(wait)
+        if do_inp:
+            input()
+
+def clear_one_line() -> None:
+    out("\033[F\033[K", end='')
+
+def clear_two_lines() -> None:
+    out("\033[F\033[K\033[F\033[K", end='')
+
+def inp_int(n: None | int = None) -> int:
+
+    err_msg = error_messages["expected_integer"]
+    if n:
+        err_msg += error_messages["expected_integer_range"].format(num1="1", num2=n)
+    err_msg += "."
+    clear = False
+
+    while True:
+        try:
+            inp = input()
+            clear_one_line()
+            if clear:
+                clear_one_line()
+            inp = int(inp)
+        except Exception:
+            out(err_msg)
+            clear = True
+            continue
+        if n:
+            if inp < 1 or inp > n:
+                out(err_msg)
+                clear = True
+                continue
+        break
+    print("{ ", inp, " }")
+    return inp
+
+def choice(options: dict) -> str:
+    options = copy.deepcopy(options)
+    if "q" in options:
+        print(options["q"])
+        options.pop("q")
+
+    for i, k in enumerate(options):
+        out(f'[{i+1}] ', options[k]["o"])
+        sleep(WAIT_TINY)
+    ch = inp_int(n=len(options))
+    i = ch - 1
+    l = list(options.values())
+    if "r" in l[i]:
+        out(l[i]["r"])
+        sleep(WAIT_NORMAL)
+    return ch
--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    
+from engine import *
+import yaml
 
+def load_dialogue(file_path: str) -> dict:
+    with open(file_path, 'r') as file:
+        dialogue = yaml.safe_load(file)
+    return dialogue
+
+DIALOGUE_PATH = "dutch.yaml"
+D = load_dialogue(DIALOGUE_PATH)
+set_error_messages(D["errors"])
+
+def what_to_do():
+    while True:
+        ch = choice(D["beginning"]["what_to_do"])
+        if ch == 1:
+            choice(D["beginning"]["look_fingers"])
+        elif ch == 2:
+            choice(D["beginning"]["wake_up"])
+        else:
+            return
+
+def main():
+    play_scene(D["beginning"]["initial"])
+    what_to_do()
+    play_scene(D["end"]["end"])
 
 if __name__  == "__main__":
     main()
